1f922e663c90f940447717b9a1577d917db45083
Kousikjami <121508643+Kousikjami@users.noreply.github.com>
2023-02-23 21:24:57 +0530

    Time: 2892 ms (47.01%), Space: 104.2 MB (37.12%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.py
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.py
@@ -5,37 +5,17 @@
 #         self.next = next
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # dummy=tail=ListNode()
-        # cur=head
-        # cur=cur.next
-        # res=0
-        # while cur:
-        #     if cur.val==0:
-        #         tail.next=dummy
-        #         res=0
-        #     else:
-        #         res=res+cur.val
-        #         dummy.val=res
-        #         dummy.next=None
-        #     cur=cur.next
-        # return tail.next
-        # brute force approach
-        res=[]
-        x=0
-        cur=head.next 
+        dummy=ListNode(0)
+        tail=dummy
+        #cur=head
+        cur=head.next
+        res=0
         while cur:
             if cur.val==0:
-                res.append(x)
-                x=0
+                tail.next=ListNode(res)
+                tail=tail.next
+                res=0
             else:
-                x+=cur.val
-            #res.append(cur.val)
+                res+=cur.val
             cur=cur.next
-        dummy=ListNode(0)
-        cur=dummy
-        for i in res:
-            cur.next=ListNode(i)
-            cur=cur.next 
         return dummy.next
-        #print(res)
-        
