bb7173a2b40adaec02ab0d69c45190b3b5329453
Kousikjami <121508643+Kousikjami@users.noreply.github.com>
2023-02-26 15:04:08 +0530

    Time: 35 ms (47.93%), Space: 13.8 MB (47.87%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        def decimalToBinary(num,base):
+            if num == 0: 
+                return 0
+            else:
+                return str(decimalToBinary(num//base,base)) + str((num%base))
+        count=0
+        for i in range(2,n-1):
+            x=str(int(decimalToBinary(n,i)))
+            if x==x[::-1]:
+                count+=1
+            else:
+                return False
+        if count==n:
+            return True
+        return False
