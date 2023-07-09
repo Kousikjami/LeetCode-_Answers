3d9a8fa570a4855a4362351289ca02dfbdd3b70f
Kousikjami <121508643+Kousikjami@users.noreply.github.com>
2023-07-09 11:45:40 +0530

    Time: 71 ms (76.14%), Space: 18.5 MB (21.60%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,11 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         d={}
-        for i in range(len(nums)):
-            if nums[i] not in d:
-                d[nums[i]]=1
+        for i in nums:
+            if i not in d:
+                d[i]=1
             else:
-                d[nums[i]]+=1
+                d[i]+=1
         for k,v in d.items():
             if v==1:
                 return k
